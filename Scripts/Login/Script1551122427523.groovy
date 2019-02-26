f2e25617bac02abc38a0f620e178d47fdfde6a17
Angel <angelantonio3000@gmail.com>
2019-02-26 03:15:03 -0400

    Conectado la base de datos con el caso de prueba y creado un suite que permite correrlo tanta veces datos haya en la consulta

--- Scripts/Login/Script1551122427523.groovy
+++ Scripts/Login/Script1551122427523.groovy
@@ -12,44 +12,65 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import testlink.api.java.client.TestLinkAPIClient
-import testlink.api.java.client.TestLinkAPIException
-import testlink.api.java.client.TestLinkAPIResults
+import testlink.api.java.client.TestLinkAPIClient as TestLinkAPIClient
+import testlink.api.java.client.TestLinkAPIException as TestLinkAPIException
+import testlink.api.java.client.TestLinkAPIResults as TestLinkAPIResults
 
 public class AutomatedUpdateExample {
-	public static String DEVKEY="f7a213e815b5649bcd5ee4ef350b3992";
+	
+	public static String DEVKEY="1831d2a0d3ffaeb921542bddf0621779"; //Colocar key del testlink personal
+	
 	public static String URL="http://localhost/testlink/lib/api/xmlrpc/v1/xmlrpc.php";
+	
 	public static void reportResult(String TestProject,String TestPlan,String Testcase,String Build,String Notes,String Result) throws TestLinkAPIException{
 	TestLinkAPIClient api=new TestLinkAPIClient(DEVKEY, URL);
 	api.reportTestCaseResult(TestProject, TestPlan, Testcase, Build, Notes, Result);
 	}
-}
-public void Test()throws Exception{
-	AutomatedUpdateExample a=new AutomatedUpdateExample();
-	String testProject="Proyecto QA";
-	String testPlan="Plan";
-	String testCase="Login";
-	String build="Build";
-	String result=null;
-	String notes=null;
-	try{
-    WebUI.openBrowser('')
-    WebUI.navigateToUrl('http://localhost:50447/Views/Login.aspx')
-    WebUI.setText(findTestObject('Page_Login  inResorts/input_Usuario_txtUsuario'), 'usera')
-    WebUI.setEncryptedText(findTestObject('Page_Login  inResorts/input_Contrasea_txtPassword'), 'FVZhtmnLu/c=')
-    WebUI.click(findTestObject('Page_Login  inResorts/input_Recordar Contrasea_btnLo'))
-    WebUI.click(findTestObject('Object Repository/Page_Inicio  inResorts/img_Salir_img-responsive'))
-    WebUI.closeBrowser()
-    result= TestLinkAPIResults.TEST_PASSED;
-    notes="Executed successfully";
-    }catch(Exception e){
-    result=TestLinkAPIResults.TEST_FAILED;
-    notes="Execution failed";
-    }
-    finally{
-	a.reportResult(testProject, testPlan, testCase, build, notes, result);
-	}
-    }
+}	
 
-    Test();
+Test()
+
+void Test() throws Exception {
+    AutomatedUpdateExample a = new AutomatedUpdateExample()
+
+    String testProject = 'Proyecto QA'
+
+    String testPlan = 'Plan'
+
+    String testCase = 'Login'
+
+    String build = 'Build'
+
+    String result = null
+
+    String notes = null
+
+    try {
+        WebUI.openBrowser('')
+
+        WebUI.navigateToUrl('http://localhost:50447/Views/Login.aspx')
+
+        WebUI.setText(findTestObject('Page_Login  inResorts/input_Usuario_txtUsuario'), usernameV)
+
+        WebUI.setEncryptedText(findTestObject('Page_Login  inResorts/input_Contrasea_txtPassword'), passwordV)
+
+        WebUI.click(findTestObject('Page_Login  inResorts/input_Recordar Contrasea_btnLo'))
+
+        WebUI.click(findTestObject('Object Repository/Page_Inicio  inResorts/img_Salir_img-responsive'))
+
+        WebUI.closeBrowser()
+
+        result = TestLinkAPIResults.TEST_PASSED
+
+        notes = 'Executed successfully'
+    }
+    catch (Exception e) {
+        result = TestLinkAPIResults.TEST_FAILED
+
+        notes = 'Execution failed'
+    } 
+    finally { 
+        a.reportResult(testProject, testPlan, testCase, build, notes, result)
+    }
+}
 
---
+++ Test Suites/Suite de Pruebas.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite de Pruebas</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f1d35204-ffd4-4e66-8aab-041e0afde8a0</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d3bebd3-83b1-4356-9e87-9a3465ea32b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>7ed4df50-5cdd-4b4d-af94-a2ee8c3a7fb0</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Login Test Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>7ed4df50-5cdd-4b4d-af94-a2ee8c3a7fb0</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>111dd1ad-d809-4fe3-9f33-4759b9ab6ca7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>7ed4df50-5cdd-4b4d-af94-a2ee8c3a7fb0</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>f8c37a57-7116-4cca-b74f-2ba65f9d4486</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
